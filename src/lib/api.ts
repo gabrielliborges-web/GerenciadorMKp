3ad8dd705def2fc65a8beae6559f0cefbcd8311f
gabrielli.melo <gabrielli.melo@simbiox.com.br>
2025-12-13 23:06:17 -0300

    url api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: "https://gerenciadormkp-api.onrender.com",
+  // baseURL: "http://localhost:4000",
+  baseURL: "https://gerenciadormkp-api.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
